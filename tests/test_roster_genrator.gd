404a0343f69158d7f9dfd06382685db7362e679c
Impendable <105776541+Impendable@users.noreply.github.com>
2026-09-03 11:21:14 -0400

    fix: roster generator output formatted properly, now only one carrier of hidden gene

--- tests/test_roster_genrator.gd
+++ tests/test_roster_genrator.gd
@@ -3,13 +3,19 @@ extends Node
 func _ready() -> void:
 	var trait_list: TraitRegistry = load("res://resources/trait_registry.tres")
 	var rng := RandomNumberGenerator.new()
-	var test_seed := 24680
+	var test_seed := 36900
 	rng.seed = test_seed
 	
 	var roster := RosterGenerator.generate(trait_list, rng)
 	
 	for fish in roster:
-		print(fish.genome.alleles)
+		print("ID: %s %s %s | color: %s%s -> %s | fin length: %s%s -> %s | glow: %s%s -> %s | growth: %s%s -> %s" % [
+			fish.id, FishData.Sex.find_key(fish.sex), FishData.LifeStage.find_key(fish.life_stage),
+			fish.genome.alleles["color"][0], fish.genome.alleles["color"][1], fish.cached_phenotype_dictionary["color"],
+			fish.genome.alleles["fin length"][0], fish.genome.alleles["fin length"][1], fish.cached_phenotype_dictionary["fin length"],
+			fish.genome.alleles["glow"][0], fish.genome.alleles["glow"][1], fish.cached_phenotype_dictionary["glow"],
+			fish.genome.alleles["growth"][0], fish.genome.alleles["growth"][1], fish.cached_phenotype_dictionary["growth"],
+		])
 	print("RNG seed(ROSTERGENTEST): ", rng.seed)
 
 #First Output:
@@ -33,4 +39,30 @@ RNG seed(ROSTERGENTEST): 13579
 { "color": ["G", "G"], "fin length": ["l", "l"], "glow": ["B", "B"], "growth": ["f", "F"] }
 { "color": ["G", "g"], "fin length": ["l", "L"], "glow": ["B", "B"], "growth": ["f", "F"] }
 RNG seed(ROSTERGENTEST): 24680
+'''
+#Output with same seed 1:
+'''
+ID: fish_0 FEMALE ADULT | color: gg -> Silver | fin length: LL -> Long | glow: Bb -> Non-glow | growth: ff -> Slow
+ID: fish_1 MALE ADULT | color: GG -> Gold | fin length: Ll -> Long | glow: BB -> Non-glow | growth: fF -> Fast
+ID: fish_2 FEMALE ADULT | color: gg -> Silver | fin length: Ll -> Long | glow: BB -> Non-glow | growth: ff -> Slow
+ID: fish_3 MALE ADULT | color: GG -> Gold | fin length: Ll -> Long | glow: BB -> Non-glow | growth: Ff -> Fast
+ID: fish_4 FEMALE ADULT | color: gG -> Gold | fin length: lL -> Long | glow: BB -> Non-glow | growth: fF -> Fast
+ID: fish_5 MALE ADULT | color: Gg -> Gold | fin length: Ll -> Long | glow: BB -> Non-glow | growth: fF -> Fast
+ID: fish_6 FEMALE ADULT | color: gG -> Gold | fin length: ll -> Short | glow: BB -> Non-glow | growth: ff -> Slow
+ID: fish_7 FEMALE ADULT | color: GG -> Gold | fin length: Ll -> Long | glow: BB -> Non-glow | growth: FF -> Fast
+ID: fish_8 FEMALE ADULT | color: gG -> Gold | fin length: Ll -> Long | glow: BB -> Non-glow | growth: fF -> Fast
+RNG seed(ROSTERGENTEST): 36900
+'''
+#Output with same seed 2:
+'''
+ID: fish_0 FEMALE ADULT | color: gg -> Silver | fin length: LL -> Long | glow: Bb -> Non-glow | growth: ff -> Slow
+ID: fish_1 MALE ADULT | color: GG -> Gold | fin length: Ll -> Long | glow: BB -> Non-glow | growth: fF -> Fast
+ID: fish_2 FEMALE ADULT | color: gg -> Silver | fin length: Ll -> Long | glow: BB -> Non-glow | growth: ff -> Slow
+ID: fish_3 MALE ADULT | color: GG -> Gold | fin length: Ll -> Long | glow: BB -> Non-glow | growth: Ff -> Fast
+ID: fish_4 FEMALE ADULT | color: gG -> Gold | fin length: lL -> Long | glow: BB -> Non-glow | growth: fF -> Fast
+ID: fish_5 MALE ADULT | color: Gg -> Gold | fin length: Ll -> Long | glow: BB -> Non-glow | growth: fF -> Fast
+ID: fish_6 FEMALE ADULT | color: gG -> Gold | fin length: ll -> Short | glow: BB -> Non-glow | growth: ff -> Slow
+ID: fish_7 FEMALE ADULT | color: GG -> Gold | fin length: Ll -> Long | glow: BB -> Non-glow | growth: FF -> Fast
+ID: fish_8 FEMALE ADULT | color: gG -> Gold | fin length: Ll -> Long | glow: BB -> Non-glow | growth: fF -> Fast
+RNG seed(ROSTERGENTEST): 36900
 '''
